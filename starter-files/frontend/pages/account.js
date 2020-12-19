3053d358ad5bbcdda69bbb4243e4d892c4c5434b
Peter Harding <plh@performiq.com>
2020-12-20 10:48:48 +1100

    Setting up backend.

---
+++ starter-files/frontend/pages/account.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Account = () => {
+    return (
+     <div>
+      <p>Account!</p>
+    </div>
+    );
+}
+
+export default Account;
---
+++ starter-files/frontend/pages/items.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Items = () => {
+    return (
+     <div>
+      <p>Items!</p>
+    </div>
+    );
+}
+
+export default Items;
